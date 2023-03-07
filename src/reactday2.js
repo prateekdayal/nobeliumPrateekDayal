a703fe065553a850ae062a60e36d13b694c17325
prateekdayal <98962176+prateekdayal@users.noreply.github.com>
2023-03-07 10:45:09 +0530

    assigment

---
+++ src/reactday2.js
@@ -0,0 +1,19 @@
+
+export function  HeadingOne(){
+    return <h1>I am heading one ... from first component</h1>;
+}
+export function  HeadingTwo(){
+    return <h2>I am heading two ... from second component</h2>;
+}
+export function  HeadingThree(){
+    return <h3>I am heading three ... from third component</h3>;
+}
+export function Button(){
+    return <button>click me!</button>
+
+}
+
+
+
+
+
